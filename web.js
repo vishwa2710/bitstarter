7d29002b6c7929727face5732ad3215a5dd7e9a2
Philip <philip@philip-ThinkPad-SL410.(none)>
2013-07-05 18:26:38 -0500

    reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
+var buff = require('buffer');
 
 var app = express.createServer(express.logger());
 var buffer = new Buffer(100);
-buffer = fs.readFileSync(index.html);
-var hel_world = buffer.toStrong();
+buffer = fs.readFileSync('index.html');
+var hel_world = buffer.toString();
 app.get('/', function(request, response) {
   response.send(hel_world);
 });
